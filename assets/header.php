721ced244d549921505317bce0e311bd8d3c4494
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-11-30 13:25:18 +0100

    přidaný general.css na stylování obecných stylů na stránkách

--- assets/header.php
+++ assets/header.php
@@ -1,4 +1,7 @@
 <header>
+    <div class="logo">
+        <img src="img/hogwarts-logo.png" alt="">
+    </div>
     <nav>
         <ul>
             <li><a href="index.php">Úvodní strana</a></li>
@@ -6,4 +9,8 @@
             <li><a href="add-student.php">Přidat žáka</a></li>
         </ul>
     </nav>
+    <div class ="menu-icon">
+		<i class="fa-solid fa-bars"></i>
+        <!-- <i class="fa-solid fa-xmark"></i> -->
+    </div>
 </header>
--- index.php
+++ index.php
@@ -7,23 +7,26 @@
 		<head>
 				<meta charset="UTF-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link rel="stylesheet" href="css/header.css">
+				<link rel="stylesheet" href="css/general.css">
+				<script src="https://kit.fontawesome.com/830a127f42.js" crossorigin="anonymous"></script>
 				<title>Document</title>
 		</head>
 		<body>
 
 		<?php require "assets/header.php" ?>
 
-				<main>
-						<section class="main-heading">
-						<h1>Škola čar a kouzel v Bradavicích</h1>
-						</section>
-						
-						<section>
-								<a href="zaci.php">Seznam všech žáků</a>
-						</section>
+				<main>				
+					<section class="main-heading">
+					<h1>Škola čar a kouzel v Bradavicích</h1>
+					</section>
+					
+					<section>
+							<a href="zaci.php">Seznam všech žáků</a>
+					</section>
 				</main>
 
 			 <?php require "assets/footer.php" ?>
-				
+			<script ></script>	
 		</body>
 		</html>
