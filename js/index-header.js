1014e988b8006fceee0f65fe7e6e66071bf3faca
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-11-30 14:26:04 +0100

    zajíždění a vyjíždění navigace po kliknutí

--- js/index-header.js
+++ js/index-header.js
@@ -1,2 +1,18 @@
+//Výběr HTML tagů se, kterými budu pracovat
+
 const menuIcon = document.querySelector(".menu-icon")
+const navigation = document.querySelector("nav")
+const hambungerIcon = document.querySelector(".fa-solid")
+
+menuIcon.addEventListener("click", () => {
+    if(hambungerIcon.classList[1] === "fa-bars") {
+        hambungerIcon.classList.remove("fa-bars")
+        hambungerIcon.classList.add("fa-xmark")
+        navigation.style.display = "block"
+    } else {
+        hambungerIcon.classList.remove("fa-xmark")
+        hambungerIcon.classList.add("fa-bars")
+        navigation.style.display = "none"
+    }
+})
 
