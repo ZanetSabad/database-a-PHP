c54d42877dd6b95b5e86aea3e00950da587e6ac6
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-12-13 22:26:15 +0100

    Upravení napojení na příslušné soubory a daná nová hlavička pro administraci (admin-header.php)

--- admin/add-student.php
+++ admin/add-student.php
@@ -1,7 +1,7 @@
 <?php 
 
-    require "assets/database.php";
-    require "assets/student.php";
+    require "../assets/database.php";
+    require "../assets/student.php";
 
     $connection = connectionDB();
 
@@ -30,26 +30,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/general.css">
-    <link rel="stylesheet" href="query/header-query.css">
-    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../query/header-query.css">
+    <link rel="stylesheet" href="../css/footer.css">
     <script src="https://kit.fontawesome.com/830a127f42.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
-    <?php require "assets/header.php" ?>
+    <?php require "../assets/admin-header.php" ?>
 
     <main>
         <section class="add-form">
             
-            <?php require "assets/form-student.php"; ?>
+            <?php require "../assets/form-student.php"; ?>
 
         </section>
     </main>
 
 
-    <?php require "assets/footer.php" ?>
-    <script src="js/index-header.js"></script>
+    <?php require "../assets/footer.php" ?>
+    <script src="../js/index-header.js"></script>
 </body>
 </html>
