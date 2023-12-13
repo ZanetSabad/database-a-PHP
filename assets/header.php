d7d534d65ade306d893c161ec4c9f571767350f0
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-12-13 22:25:31 +0100

    header.php - hlavička pro úvodní stranu, předělána, tak aby tak nebyly složky, kteréí máme v administraci

--- assets/header.php
+++ assets/header.php
@@ -8,8 +8,6 @@
     <nav>
         <ul>
             <li><a href="index.php">Úvodní strana</a></li>
-            <li><a href="zaci.php">Seznam žáků</a></li>
-            <li><a href="add-student.php">Přidat žáka</a></li>
         </ul>
     </nav>
     <div class ="menu-icon">
