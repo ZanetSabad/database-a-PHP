19d0f86d84c43c5cba215c38f27d66d6dfc3e275
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-12-14 13:33:53 +0100

    hashování hesla

--- admin/after-registration.php
+++ admin/after-registration.php
@@ -12,7 +12,13 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
     $first_name = $_POST["first_name"];
     $second_name = $_POST["second_name"];
     $email = $_POST["email"];
-    $password = $_POST["password"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    // Plain text = admin123
+    // Hash = nfdjeklff564345dnjhkgshjdbjh
+
+    
+
 
     var_dump($first_name, $second_name, $email, $password);
 }
