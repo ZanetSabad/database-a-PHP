c5371f7f01dc6f87998cf5234621e7c4196fa614
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-10 19:52:59 +0100

    založení login.php a základní nastavení

---
+++ admin/login.php
@@ -0,0 +1,18 @@
+<?php
+
+require "../assets/database.php";
+require "../assets/url.php";
+require "../assets/user.php";
+
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $conn = connectionDB();
+    $log_email = $_POST["login-email"];
+    $log_password = $_POST["login-password"];
+
+    var_dump($log_email);
+
+
+}
